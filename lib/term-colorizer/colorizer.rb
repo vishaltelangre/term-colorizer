838cc7c6cdf87bc6922bf7b4dd86acae37369fd6
Vishal Telangre <the@vishaltelangre.com>
2013-07-13 20:50:58 +0530

    dynamically define color method just before calling it

--- lib/term-colorizer/colorizer.rb
+++ lib/term-colorizer/colorizer.rb
@@ -9,28 +9,26 @@ module Term
     def method_missing(sym, *args, &block)
       method = sym.to_s
       super unless TERM_COLORS.include? method
-      self.send(method)
-    end
-
-    TERM_COLORS.each do |color|
-      case color
-      when "black" then define_method("black") { "\e[30m" + self.to_s + "\e[0m" }
-      when "red" then define_method("red") { "\e[31m" + self.to_s + "\e[0m" }
-      when "green" then define_method("green") { "\e[32m" + self.to_s + "\e[0m" }
-      when "yellow" then define_method("yellow") { "\e[33m" + self.to_s + "\e[0m" }
-      when "blue" then define_method("blue") { "\e[34m" + self.to_s + "\e[0m" }
-      when "magenta" then define_method("magenta") { "\e[35m" + self.to_s + "\e[0m" }
-      when "cyan" then define_method("cyan") { "\e[36m" + self.to_s + "\e[0m" }
-      when "white" then define_method("white") { "\e[37m" + self.to_s + "\e[0m" }
-      when "bright_black" then define_method("bright_black") { "\e[1m\e[30m" + self.to_s + "\e[0m" }
-      when "bright_red" then define_method("bright_red") { "\e[1m\e[31m" + self.to_s + "\e[0m" }
-      when "bright_green" then define_method("bright_green") { "\e[1m\e[32m" + self.to_s + "\e[0m" }
-      when "bright_yellow" then define_method("bright_yellow") { "\e[1m\e[33m" + self.to_s + "\e[0m" }
-      when "bright_blue" then define_method("bright_blue") { "\e[1m\e[34m" + self.to_s + "\e[0m" }
-      when "bright_magenta" then define_method("bright_magenta") { "\e[1m\e[35m" + self.to_s + "\e[0m" }
-      when "bright_cyan" then define_method("bright_cyan") { "\e[1m\e[36m" + self.to_s + "\e[0m" }
-      when "bright_white" then define_method("bright_white") { "\e[1m\e[37m" + self.to_s + "\e[0m" }
-      end
+      self.class.send(:define_method, method) do
+        case method
+        when "black" then "\e[30m" + self.to_s + "\e[0m"
+        when "red" then "\e[31m" + self.to_s + "\e[0m"
+        when "green" then "\e[32m" + self.to_s + "\e[0m"
+        when "yellow" then "\e[33m" + self.to_s + "\e[0m"
+        when "blue" then "\e[34m" + self.to_s + "\e[0m"
+        when "magenta" then "\e[35m" + self.to_s + "\e[0m"
+        when "cyan" then "\e[36m" + self.to_s + "\e[0m"
+        when "white" then "\e[37m" + self.to_s + "\e[0m"
+        when "bright_black" then "\e[1m\e[30m" + self.to_s + "\e[0m"
+        when "bright_red" then "\e[1m\e[31m" + self.to_s + "\e[0m"
+        when "bright_green" then "\e[1m\e[32m" + self.to_s + "\e[0m"
+        when "bright_yellow" then "\e[1m\e[33m" + self.to_s + "\e[0m"
+        when "bright_blue" then "\e[1m\e[34m" + self.to_s + "\e[0m"
+        when "bright_magenta" then "\e[1m\e[35m" + self.to_s + "\e[0m"
+        when "bright_cyan" then "\e[1m\e[36m" + self.to_s + "\e[0m"
+        when "bright_white" then "\e[1m\e[37m" + self.to_s + "\e[0m"
+        end
+      end and self.send(method, *args)
     end
   end
 end
